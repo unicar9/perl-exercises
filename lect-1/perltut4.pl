a59c463cba5ce51d1a770acc615d98679a775980
Weijia Li <unicar9@gmail.com>
2019-09-16 16:12:55 +1000

    added perltut4 solution

--- lect-1/perltut4.pl
+++ lect-1/perltut4.pl
@@ -1,3 +1,13 @@
 # (4) Write a perl program that prompts the user for a temperature in Celsius and prints its value in Fahrenheit. The conversion formula is
 
 # Fahrenheit = ( 9*Celsius/5 ) + 32
+
+#!/usr/bin/perl -w
+
+print "Please enter a temperature in Celsius: ";
+$celsius = <STDIN>;
+chomp $celsius;
+
+$fahrenheit = (9 * $celsius / 5) + 32;
+
+print "The temperature in Fahrenheit is $fahrenheit degree.\n";
