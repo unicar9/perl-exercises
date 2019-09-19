40e0443299129d1b2491d422ca49f774aeb1e2f2
Unicar <unicar9@gmail.com>
2019-09-19 22:08:18 +1000

    added perltut10.pl

--- lect-1/perltut10.pl
+++ lect-1/perltut10.pl
@@ -1,1 +1,9 @@
 # (10) Write a perl program that takes a text file name as an argument on the command line. The program should open the file and write its contents to the screen. If the file does not exist the program should print “File not found” and exit.
+
+#!/usr/bin/perl -w
+
+open (FILE, $ARGV[0]) or die "File not found";
+
+while (my $line = <FILE>) {
+  print $line;
+}
