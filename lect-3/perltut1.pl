ae4526cf826c2d04d263f6522a58041d79815dbd
Unicar <unicar9@gmail.com>
2019-10-10 14:33:37 +1100

    Updated tut1

--- lect-3/perltut1.pl
+++ lect-3/perltut1.pl
@@ -1,1 +1,13 @@
 # (1) Write a program that goes through the file india_captains.txt line by line and just prints the names of the captains. Hint: Start by putting the data from each line into an array using the perl split command.
+
+#!/usr/bin/perl -w
+
+use strict;
+
+open (FILE, "./india_captains.txt") or die "$!";
+
+while(my $current_line = <FILE>) {
+  chomp($current_line);
+  my @data_fields = split ",", $current_line;
+  print $data_fields[0]."\n"
+}
