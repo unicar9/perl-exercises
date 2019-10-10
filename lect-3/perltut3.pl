448a918ff306cb60a0a99ffa78ffb64deda84425
Unicar <unicar9@gmail.com>
2019-10-10 14:39:01 +1100

    Updated tut3

--- lect-3/perltut3.pl
+++ lect-3/perltut3.pl
@@ -1,1 +1,13 @@
 # (3) Make a copy of your working answer from (1). Modify the file so the names of the captains are printed right justified in a space 30 characters wide. Hint: look at the perl printf command.
+
+#!/usr/bin/perl -w
+
+use strict;
+
+open (FILE, "./india_captains.txt") or die "$!";
+
+while (my $current_line = <FILE>) {
+  chomp($current_line);
+  my @data_fields = split ",", $current_line;
+  printf("%30s\n", $data_fields[0]);
+}
